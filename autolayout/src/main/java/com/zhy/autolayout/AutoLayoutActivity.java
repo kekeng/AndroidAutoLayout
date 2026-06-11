8414a531dafbb55d5616baad267439d4117f7732
kk83773823 <kk83773823@gmail.com>
2026-06-11 15:41:39 +0800

    修改支持RadioGroup

--- autolayout/src/main/java/com/zhy/autolayout/AutoLayoutActivity.java
+++ autolayout/src/main/java/com/zhy/autolayout/AutoLayoutActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.View;
 
-;
-
 /**
  * Created by zhy on 15/11/19.
  */
@@ -20,6 +18,8 @@ public class AutoLayoutActivity extends AppCompatActivity
     private static final String LAYOUT_SCROLLVIEW = "ScrollView";
     private static final String LAYOUT_HORIZONTALSCROLLVIEW = "HorizontalScrollView";
     private static final String LAYOUT_NESTEDSCROLLVIEW = "androidx.core.widget.NestedScrollView";
+    private static final String LAYOUT_RADIOGROUP = "RadioGroup";
+    private static final String LAYOUT_RADIOGROUP_FULL = "android.widget.RadioGroup";
 
 
     @Override
@@ -64,6 +64,11 @@ public class AutoLayoutActivity extends AppCompatActivity
         if (name.equals(LAYOUT_NESTEDSCROLLVIEW))
         {
             view = new AutoNestedScrollView(context, attrs);
+        }
+
+        if (name.equals(LAYOUT_RADIOGROUP) || name.equals(LAYOUT_RADIOGROUP_FULL))
+        {
+            view = new AutoRadioGroup(context, attrs);
         }
 
         if (view != null) return view;
---
+++ autolayout/src/main/java/com/zhy/autolayout/AutoRadioGroup.java
@@ -0,0 +1,85 @@
+package com.zhy.autolayout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+
+import com.zhy.autolayout.utils.AutoLayoutHelper;
+
+public class AutoRadioGroup extends RadioGroup
+{
+    private AutoLayoutHelper mHelper = new AutoLayoutHelper(this);
+
+    public AutoRadioGroup(Context context)
+    {
+        super(context);
+    }
+
+    public AutoRadioGroup(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public AutoRadioGroup(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AutoRadioGroup(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes)
+    {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        if (!isInEditMode())
+            mHelper.adjustChildren();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs)
+    {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    public static class LayoutParams extends RadioGroup.LayoutParams
+            implements AutoLayoutHelper.AutoLayoutParams
+    {
+        private AutoLayoutInfo mAutoLayoutInfo;
+
+        public LayoutParams(Context c, AttributeSet attrs)
+        {
+            super(c, attrs);
+            mAutoLayoutInfo = AutoLayoutHelper.getAutoLayoutInfo(c, attrs);
+        }
+
+        @Override
+        public AutoLayoutInfo getAutoLayoutInfo()
+        {
+            return mAutoLayoutInfo;
+        }
+
+        public LayoutParams(int width, int height)
+        {
+            super(width, height);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source)
+        {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source)
+        {
+            super(source);
+        }
+    }
+}
+
